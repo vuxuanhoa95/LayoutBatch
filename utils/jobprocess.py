a5033544e49b7edac323337eb0aab7aa61d513ab
vuxuanhoa95 <61386551+vuxuanhoa95@users.noreply.github.com>
2022-08-17 08:02:59 +0700

    update process

--- utils/jobprocess.py
+++ utils/jobprocess.py
@@ -2,6 +2,7 @@ import logging
 import subprocess
 import threading
 import os
+import sys
 
 TEMP = r'D:\temp'
 LOG_FILE = os.path.join(TEMP, 'test.log')
@@ -19,7 +20,7 @@ def call(*args):
 
     def run_in_thread(arguments, on_exit, on_progress, on_log):
 
-        proc = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+        proc = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                                 creationflags=subprocess.CREATE_NEW_CONSOLE)
 
         def check_io():
@@ -31,8 +32,10 @@ def call(*args):
                 on_progress(i)  # progress callback
                 output = proc.stdout.readline().decode()
                 if output:
-                    # logger.log(logging.INFO, output)
+                    logger.log(logging.INFO, output)
+                    sys.stdout.write(output)
                     on_log(output[:63])  # log callback
+
                 else:
                     break
 
